0ede47367c8f5ca9f8210453cb1e87fba7ee4f57
Mr.lu <sekift@163.com>
2018-06-04 17:45:57 +0800

    更新简介
    更新简介

--- aurochs-server/src/test/java/cn/aurochs/www/cluster/BuildClusterTest.java
+++ aurochs-server/src/test/java/cn/aurochs/www/cluster/BuildClusterTest.java
@@ -9,12 +9,13 @@ public class BuildClusterTest {
 //				"b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19,b20,b21,b22,b23,b24,b25,b26,b27,b28,b29,b30,b31,b32,b33,b34,b35,b36,b37,b38,b39,b40,b41,b42,b43,b44,b45,b46,b47,b48,b49,b50;b51,b52,b53,b54,b55,b56,b57,b58,b59,b60,b61,b62,b63,b64,b65,b66,b67,b68,b69,b70,b71,b72,b73,b74,b75,b76,b77,b78,b79,b80,b81,b82,b83,b84,b85,b86,b87,b88,b89,b90,b91,b92,b93,b94,b95,b96,b97,b98,b99,b100;" +
 //				"s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19,s20,s21,s22,s23,s24,s25,s26,s27,s28,s29,s30,s31,s32,s33,s34,s35,s36,s37,s38,s39,s40,s41,s42,s43,s44,s45,s46,s47,s48,s49,s50;s51,s52,s53,s54,s55,s56,s57,s58,s59,s60,s61,s62,s63,s64,s65,s66,s67,s68,s69,s70,s71,s72,s73,s74,s75,s76,s77,s78,s79,s80,s81,s82,s83,s84,s85,s86,s87,s88,s89,s90,s91,s92,s93,s94,s95,s96,s97,s98,s99,s100;" +
 //				"h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11,h12,h13,h14,h15,h16,h17,h18,h19,h20,h21,h22,h23,h24,h25,h26,h27,h28,h29,h30,h31,h32,h33,h34,h35,h36,h37,h38,h39,h40,h41,h42,h43,h44,h45,h46,h47,h48,h49,h50;h51,h52,h53,h54,h55,h56,h57,h58,h59,h60,h61,h62,h63,h64,h65,h66,h67,h68,h69,h70,h71,h72,h73,h74,h75,h76,h77,h78,h79,h80,h81,h82,h83,h84,h85,h86,h87,h88,h89,h90,h91,h92,h93,h94,h95,h96,h97,h98,h99,h100";//
-		String machineZone = "b1,b2,b3,b4;g1,g2,g3,g4;s1,s2,s3,s4";//;h1,h2,h3,h4;x1,x2,x3,x4
+		String machineZone = "b1,b2,b3,b4,b5,b6,b7,b8;g1,g2,g3,g4,g5,g6,g7,g8;s1,s2,s3,s4,s5,s6,s7,s8;h1,h2,h3,h4,h5,h6,h7,h8";//;h1,h2,h3,h4;x1,x2,x3,x4
 //		String machineZone = "g1,g2,g2,g2,g2,g2,g2,g2,g2,g2,g2,g2,g2,g2,g2,g2";//
 		Map<String, Integer> map = new TreeMap<String, Integer>();
-		map.put("cluster1", 4);
-		map.put("cluster2", 4);
-		map.put("cluster3", 4);
+		map.put("cluster1", 12);
+		map.put("cluster2", 8);
+		map.put("cluster3", 8);
+		map.put("cluster4", 4);
 //		map.put("group4", 20);
 //		map.put("group5", 20);
 //		map.put("group6", 20);
